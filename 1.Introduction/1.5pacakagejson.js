87249b10a52fcbafb281cb80b43b0536f3c1e4f3
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-23 18:29:58 +0530

    Update

--- 1.Introduction/1.5pacakagejson.js
+++ 1.Introduction/1.5pacakagejson.js
@@ -2,10 +2,15 @@ console.log("Package.Json");
 
 // Package.json :- It keep the details of project in coding related like project version project name repository which pacakage you install etc
 
-const colors = require("colors"); // when we install npm i colors then we have to call that and it give color to out output we can install many modules like this.
-console.log("Hello Radhe".red); // Hello Radhe in red color
+// const colors = require("colors"); // when we install npm i colors then we have to call that and it give color to out output we can install many modules like this.
+// console.log("Hello Radhe".red); // Hello Radhe in red color
 
 
 // Node js is single threaded language.
 // JavaScript ia also  a Single Threaded Language
 // Jai Shree Radhe
+// chal Jhutha 
+// Bakri ke Ammma 
+// You are a human or piece of Goat
+
+console.log("Hello");
