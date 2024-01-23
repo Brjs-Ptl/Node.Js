5c38eec7ce204f977eb72ec34ff0d82e37ec550e
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-24 00:45:27 +0530

    import chalk and write file name .mjs instead of .js

--- 1.Introduction/1.6.js
+++
@@ -1,6 +0,0 @@
-// const chalk = require("chalk");
-
-// console.log(chalk.green("Hello Radhe"));
-
-import chalk from "chalk";
-console.log(chalk.green("Hello Radhe"));
---
+++ 1.Introduction/1.6importChalk.mjs
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+// here if you want to write in chalk format then you have 
+// to first write your file name in .mjs instead of .js
+//then call it give result.
