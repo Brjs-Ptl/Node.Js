26013d4a39d077dc6e3c6fad66077f3285bf4afb
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-24 00:59:08 +0530

    NodemonPackage install

---
+++ 1.Introduction/1.7nodemonPackage.js
@@ -0,0 +1,8 @@
+console.log("Try Nodemon in Node.js and gat result");
+console.log("Nodemon Working");
+console.log(20+30);
+
+// when we write above code and save then call in terminal it give result.
+// if i change console.log("Try Nodemon "); and save then again call and then get output
+
+// so when we install nodemon package we dont have to check again and agai it  run automatically and give output
