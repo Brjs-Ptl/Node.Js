1554012710945fafb9c8e7e41b823e8ea8b0337d
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-24 01:40:43 +0530

    Import and export data

---
+++ 1.Introduction/1.8.1makeServerImport.js
@@ -0,0 +1,32 @@
+// -----------------------------1st step----------------------------------
+
+/*
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.writeHead(200, {"content-Type":"application\json"});
+    res.write(JSON.stringify({name:"Brijesh", email:"ibrijeshpatel@outlook.com", Phone_Number: 96124}));
+    res.end();
+}).listen(5000);
+
+*/
+
+// when we open localhost:5000 in web it give {"name":"Brijesh"}
+// when we past http://localhost:5000/(copy the link from web) and pest in postman and get data it also give same result {"name":"Brijesh"}
+
+
+// -----------------------2nd step-----------------------------
+
+// if you have big data import from other file then what we have to do
+
+const http = require("http");
+const data = require("./1.8.2makeServerExport");
+
+http.createServer((req, res) => {
+    res.writeHead(200, {"content-Type":"application/json"});
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(5000);
+
+
+// it give multiple data which have written in 1.8.2makeServerExport.
---
+++ 1.Introduction/1.8.2makeServerExport.js
@@ -0,0 +1,14 @@
+// data = {name:"Brijesh", email:"ibrijeshpatel@outlook.com", Phone_Number: 96124},
+
+// module.exports = data;
+
+
+const data = [
+    {name:"Brijesh", email:"ibrijeshpatel@outlook.com", Phone_Number: 96124}, // if multiple data
+    {name:"Rohan", email:"roshan@outlook.com", Phone_Number: 96121},
+    {name:"Mohan", email:"mohan@outlook.com", Phone_Number: 96122},
+    {name:"Raghav", email:"raghav@outlook.com", Phone_Number: 96123},
+    {name:"Lalji", email:"lalji@outlook.com", Phone_Number: 96124},
+    {name:"Madhav", email:"madhav@outlook.com", Phone_Number: 96125}
+]
+module.exports = data;
