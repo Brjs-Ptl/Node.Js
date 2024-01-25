cabb2e34a4c36dd8b3059d54474b86404abef8ca
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-25 18:47:32 +0530

    Updated

--- 1.Introduction/2.0createMultipletxt.js
+++ 1.Introduction/2.0createMultipletxt.js
@@ -1,37 +1,15 @@
 // const fs = require("fs");
 // fs.writeFileSync("apple.txt","Hello Worls Again");
 
-// Above create only one file which is apple.txt
+// Above create only one file in once which is apple.txt
 
-// i try to create multiple file in one click
 
-// const fs = require("fs");
-
-// const path = require("path"); //for this we need path
-
-// const dirPath = path.join(__dirname,"files"); //path get from here but for file path we write "files" after the result
-// console.log(dirPath);
-
-// console.log(__dirname,"files"); // not working //another way to get path but for file path we write "files" after the result but it give wrong 
-
-// // Below code is not working in this computer
-// const fs = require("fs");
-// const path = require("path"); 
-// const dirPath = path.join(__dirname, "files");
-// for (let i = 0; i < 5; i++) {
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"some simple text in file");
-// }
-
-// so we try this
+// we want to create multiple file in one click
+// here we create multiple file in one file
 
 const fs = require("fs");
 const path = require("path"); 
-
-const dirPath = path.join(__dirname, "files");
-
-if (!fs.existsSync(dirPath)) { // Check if the directory exists, if not, create it
-    fs.mkdirSync(dirPath);
-}
+const dirPath = path.join(__dirname, "files");//path get from __dirname and in which file you want to write multiple file is here files
 
 for (let i = 0; i < 5; i++) {
     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"some simple text in file");
@@ -39,8 +17,7 @@ for (let i = 0; i < 5; i++) {
 
 
 // for read file directry to check what content available there
-
-
+// here we check how many file available ion specific file
 fs.readdir(dirPath, (err, files) => {
     // console.log(files); // it give array of files
     files.forEach((items)=> {    // it give normal files
