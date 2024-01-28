0fe42434c2bbe57558400b7fbc8cc0ea58327ff8
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-01-28 16:54:50 +0530

    Import Data and restriction in specific page

--- Express.Js/1.7.1.js
+++ Express.Js/1.7.1.js
@@ -1,3 +1,6 @@
+// middleware in Single Page // restriction in single page
+// reqFilter in same page
+/*
 const express = require("express");
 const path = require("path");
 
@@ -15,6 +18,40 @@ const reqFilter = (req,res,next) => {
     }
 }
 // app.use(reqFilter);
+
+// if we search http://localhost:4200/user?age=23 then it give result otherwise it demand user age
+app.get("",(req,res) => {
+    res.send(`Wow you are in Home Page`);
+});
+
+app.get("/about",(req,res) => {
+    const tag = `<h3>Welcome to About Us Page</h3>`;
+    const message = `<p> 
+    Look back on 25 years in Search history, 
+    honoring the most searched moments that have inspired the world 
+    and the next generation to come. 
+    </p>`;
+
+    res.send(`${tag} <br> ${message}`);
+});
+
+
+app.get("/user",reqFilter,(req,res) => {  //restriction only working in this page and other one is run smoothly
+    res.send(`Welcome to Users Page`);
+});
+
+app.listen(4200);
+*/
+
+
+// reqFilter import from another file // midleware working in require page
+const express = require("express");
+const reqFilter = require("./1.7.2middleware")
+
+// const htmlPath = path.join(__dirname,"ExcessHTMLcode")
+
+const app = express()
+
 
 // if we search http://localhost:4200/user?age=23 then it give result otherwise it demand user age
 app.get("",(req,res) => {
