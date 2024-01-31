4fd666dde0ae4053231e7abb12e0d4a71123bf10
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-02-01 02:34:24 +0530

    Update Multiple Data

--- NodeExpressMongo/1.2.js
+++ NodeExpressMongo/1.2.js
@@ -3,12 +3,20 @@ const mongoose = require("mongoose");
 const main = async () => {
     await mongoose.connect("mongodb://localhost:27017/e-commerse");
     const ProductSchema = new mongoose.Schema({
-        name:String,
-        price:Number,
-        
+        Name:String,
+        Price:Number,
+        Brand:String,
+        MadeIn:String,        
+        Category:String
     });
     const ProductModel = mongoose.model("products",ProductSchema);
-    let data = new ProductModel({name:"Mohan", price:1000});
+    let data = new ProductModel({
+        Name:"Zt100", 
+        Price:1000,
+        Brand:"Zen Phone",
+        MadeIn:"Bharat",
+        Category:"Mobile"
+    });
     let result = await data.save();
     console.log(result);
 };
