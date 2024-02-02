ed9ac785443429e4a746290d559b0bd496152fb3
Brjs-Ptl <ibrijeshpatel@outlook.com>
2024-02-02 21:09:19 +0530

    Upload file as .jpg through Multer

---
+++ NodeExpressMongo/1.4uploadFileMulter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+
+const uploadFile = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "1.4.2fileUploadedMulter")  // Already created file in teskbar
+        },
+        filename: function (req, file, cb)  {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single("upload_file_multer"); // through which parameter you upload file //postman_post_form-data_key:upload_file_multer_file:file_value:select-fromfile. 
+
+app.post("/upload", uploadFile, (req, res) => {
+    res.send("Uploading Done")
+});
+
+app.listen(4200); 
+
+
